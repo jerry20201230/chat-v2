790038c50e5424ff54721e5cefc8f33e5511d9db
jerry20201230 <76645282+jerry20201230@users.noreply.github.com>
2022-10-08 12:02:59 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get(/icon|canvas|public/, (req, res) => {
+  res.sendFile(`${__dirname}/${req.path}`);
+});
+
+//The 404 Route (ALWAYS Keep this as the last route)
+app.get('*', function (req, res) {
+  res.status(404).sendFile(__dirname + '/public/404.html');
+});
+
+
+io.on("connect",function(socket_id){
+    
+})
+
+
+
+
+http.listen(port, () => console.log('listening on port ' + port));
